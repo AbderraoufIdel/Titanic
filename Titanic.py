5947b3546d34d8f979a54880306eccf2214582e2
AbderraoufIdel <abderraoufidel@gmail.com>
2024-08-10 11:00:27 +0100

    Titanic Update

--- Titanic.py
+++ Titanic.py
@@ -1,1 +1,10 @@
-print("On.")
+import numpy as np
+import pandas as pd
+
+import os 
+#for dirname, _, filenames in os.walk(r"C:\Users\abder\Desktop\folder0\Titanic"):
+#   for filename in filenames:
+#        print(os.path.join(dirname, filename))
+
+train_data = pd.read_csv(r"C:\Users\abder\Desktop\folder0\Titanic\train.csv")
+train_data.head()
